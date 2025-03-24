a4a2d2bdf67a3091e1a5ff2be2fa82d79f89261d
Anders S. Østergaard <sandoe_1@hotmail.com>
2025-03-24 12:01:12 +0100

    Update 04_change_passwd.py

--- 03_mariadb_setup/04_change_passwd.py
+++ 03_mariadb_setup/04_change_passwd.py
@@ -1,1 +1,16 @@
+#Advarsel: Brug kun dette på testmaskiner. På produktionssystemer bør adgangskoder ændres manuelt og logges korrekt.
+import pymysql
 
+connection = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="gammelKode"  # ← Skift denne
+)
+
+try:
+    with connection.cursor() as cursor:
+        cursor.execute("ALTER USER 'root'@'localhost' IDENTIFIED BY 'nyKode123';")
+        print("Adgangskode ændret.")
+    connection.commit()
+finally:
+    connection.close()
